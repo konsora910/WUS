24d0cc54c6c5d52c0575b1c752b1d3a2457747eb
SeiyaTakasugi <seiya100dash@gmail.com>
2019-05-08 09:55:19 +0900

    チェックポイントの作り直し

--- WUS/WUS/Assets/script/CheckPoint.cs
+++ WUS/WUS/Assets/script/CheckPoint.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,13 +10,14 @@ public class CheckPoint : MonoBehaviour
         collision,
         trigger
     };
-
+    public float Move1;
+    public float Move2;
+    private float stMove;
     private Mode mode;
     private bool isCheck;
+    private int nCheck = 0;
     PlayerController PCont;
-    MainCam mcam;
     private bool StopCamera = false;
-    
 
     // Start is called before the first frame update
     void Start()
@@ -23,9 +25,34 @@ public class CheckPoint : MonoBehaviour
         if (mode == Mode.collision)
         {
             GetComponentInChildren<Collider2D>().isTrigger = false;
-        }else if(mode == Mode.trigger)
+        } else if (mode == Mode.trigger)
         {
             GetComponentInChildren<Collider2D>().isTrigger = true;
+        }
+        switch (nCheck)
+        {
+            case 0:
+                stMove = Move1;
+                break;
+            case 1:
+                stMove = Move2;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (StopCamera)
+        {
+            Camera.main.gameObject.transform.position += new Vector3(0.5f, 0.0f, 0.0f);
+            if (Camera.main.gameObject.transform.position.x >= stMove)
+            {
+                StopCamera = false;
+                PCont.enabled = true;
+                nCheck++;
+            }
         }
     }
 
@@ -34,10 +61,23 @@ public class CheckPoint : MonoBehaviour
         if(!isCheck)
         {
             JudgeCheck(collision.collider);
-            CameraMoveOrder();
+            StopCamera = true;
+            //CameraMoveOrder();
             Debug.Log("Collision : CheckPoint");
         }
     }
+
+    //private void CameraMoveOrder()
+    //{
+    //    if (!StopCamera)
+    //    {
+    //        StopCamera = true;
+    //    }
+    //    else if (StopCamera)
+    //    {
+    //        StopCamera = false;
+    //    }
+    //}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -54,18 +94,6 @@ public class CheckPoint : MonoBehaviour
             // キャラクターの動きを止める
             PCont = colCheck.GetComponent<PlayerController>();
             PCont.enabled = false;
-        }
-    }
-
-    void CameraMoveOrder()
-    {
-        if(StopCamera)
-        {
-            Camera.main.gameObject.transform.position += new Vector3(1.0f, 0.0f, 0f);
-            if(Camera.main.gameObject.transform.position.x>=25.0f)
-            {
-                StopCamera = true;
-            }
         }
     }
 }
