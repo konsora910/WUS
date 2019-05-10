afdaf2ff44a5827d841c901d94859f5736c6c08c
SeiyaTakasugi <seiya100dash@gmail.com>
2019-05-10 10:00:47 +0900

    サンプルシーン

---
+++ WUS/WUS/Assets/script/Respawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public float power;
+    private Transform respawnpoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawnpoint = transform.Find("Respawn");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine("ReturnCharacter", other.gameObject);
+        }
+    }
+
+    private IEnumerator ReturnCharactor(GameObject charactor)
+    {
+        yield return new WaitForSeconds(1f);
+        charactor.transform.position = respawnpoint.position;
+    }
+}
