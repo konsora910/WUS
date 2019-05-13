dc228f646936e7dd963dc9e13683dd7eae12ac8f
HagiyamaYu <49739492+HagiyamaYu@users.noreply.github.com>
2019-05-13 11:31:33 +0900

    新しい星のスクリプト

---
+++ WUS/WUS/Assets/script/CutObject.cs
@@ -0,0 +1,34 @@
+﻿//*********************************************************
+//　ギミック　ひも
+//　星(スライス)に当たったら消える
+// 要改造
+//*********************************************************
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Slice_Star")
+        {
+            BoxCollider2D Collider = GetComponent<BoxCollider2D>(); //コライダー取得
+            
+            this.gameObject.SetActive(false);
+            
+        }
+
+    }
+}
---
+++ WUS/WUS/Assets/script/SliceStar.cs
@@ -0,0 +1,35 @@
+﻿//*********************************************************
+//　星(スライス)につけるスクリプト
+//　CutObject以外に触れたら消えるだけ！
+//
+//*********************************************************
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliceStar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+  /*  // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Cut_Object" || col.gameObject.tag == "Star")
+        {
+
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
