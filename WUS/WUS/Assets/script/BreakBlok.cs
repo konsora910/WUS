9f549574deef258212e067ccf8d9e1cb72e19f74
HagiyamaYu <49739492+HagiyamaYu@users.noreply.github.com>
2019-05-15 11:31:56 +0900

    新しい星

---
+++ WUS/WUS/Assets/script/BreakBlok.cs
@@ -0,0 +1,32 @@
+﻿//=========================================================
+//      BreakBlock
+//爆発星が当たったら消えるオブジェクトにつける
+//
+//特にいじるところはない
+//=========================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakBlok : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+  /*  void Update()
+    {
+        
+    }*/
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Explosion_Star")
+        {
+            this.gameObject.SetActive(false);
+        }
+        
+    }
+}
