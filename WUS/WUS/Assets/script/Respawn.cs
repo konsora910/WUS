2e9de3c90c6ae14eb22f61fea7cf36813eeffd56
SeiyaTakasugi <seiya100dash@gmail.com>
2019-05-22 10:22:42 +0900

    コーヒーのコミット

--- WUS/WUS/Assets/script/Respawn.cs
+++ WUS/WUS/Assets/script/Respawn.cs
@@ -4,27 +4,54 @@ using UnityEngine;
 
 public class Respawn : MonoBehaviour
 {
-    public float power;
     private Transform respawnpoint;
-    public GameObject respawn;
+    private GameObject respawn;
+    private GameObject[] RespawnArray = new GameObject[3];
+
+    public int ChangeRes = 0;
     // Start is called before the first frame update
     void Start()
     {
-        respawnpoint = respawn.transform;
+        RespawnArray[0] = GameObject.Find("Respawn");
+        RespawnArray[1] = GameObject.Find("Respawn2");
+        RespawnArray[2] = GameObject.Find("Respawn3");
     }
-
+    private void Update()
+    {
+        switch (ChangeRes)
+        {
+            case 0:
+                respawn = RespawnArray[0];
+                break;
+            case 1:
+                respawn = RespawnArray[1];
+                break;
+            case 2:
+                respawn = RespawnArray[2];
+                break;
+            default:
+                break;
+        }
+    }
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Gimic")
         {
-            ReturnPoint(other.gameObject);
-            GetComponent<AudioSource>().Play();
+            GameObject pc = GetComponent<PlayerController>().gameObject;
+            ReturnPoint(pc);
+            //GetComponent<AudioSource>().Play();
         }
     }
 
     // キャラクターをリスポーン地点に戻す
     public void ReturnPoint(GameObject getChar)
     {
-        getChar.transform.position = respawnpoint.position;
+        getChar.transform.position = respawn.transform.position;
+    }
+    
+    public int NumResP()
+    {
+        ChangeRes++;
+        return ChangeRes;
     }
 }
