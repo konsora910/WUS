ed312753af1f31a496805eb3f403e56b9dc0b0f5
konsora <34297567+konsora910@users.noreply.github.com>
2019-05-23 09:29:12 +0900

    cs

---
+++ WUS/WUS/Assets/script/UI_Number.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UI_Number : MonoBehaviour
+{
+    public Sprite Sprite1;
+    public Sprite Sprite2;
+    public Sprite Sprite3;
+    public Image image;
+    GameObject player;
+    int ResCheck;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("player");    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       ResCheck =  player.GetComponent<Respawn>().GetResPNum();
+        switch (ResCheck)
+        {
+            case 0:
+                image.sprite = Sprite1;
+                break;
+            case 1:
+                image.sprite = Sprite2;
+                break;
+            case 2:
+                image.sprite = Sprite3;
+                break;
+            default:
+                break;
+        }
+
+    }
+}
