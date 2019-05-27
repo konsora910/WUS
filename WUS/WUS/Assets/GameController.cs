d0f4cc6d7da47c74d024f235aec24472fab87bd1
konsora <34297567+konsora910@users.noreply.github.com>
2019-05-27 11:48:05 +0900

    フェードmenu以外

--- WUS/WUS/Assets/GameController.cs
+++ WUS/WUS/Assets/GameController.cs
@@ -8,10 +8,17 @@ public class GameController : MonoBehaviour
     public GameObject Sceneobject;
 
     public AudioClip SelectSE;
+    GameObject Fade;
+    int FadeTimeMax = 60;
+    int FadeTime = 0;
+    bool TitleFadeCheck = false;
+    bool MenuFadeCheck = false;
+    bool TutorialFadeCheck = false;
+    bool FinishFadeCheck = false;
     // Start is called before the first frame update
     void Start()
     {
-      
+        Fade = GameObject.Find("Fade");
     }
     void Quit()
     {
@@ -26,30 +33,83 @@ public class GameController : MonoBehaviour
         {
             //UnityEditor.EditorApplication.isPlaying = false;
         }
+        if(TitleFadeCheck == true)
+        {
+            TitleFadeOut();
+        }
+
+        if (MenuFadeCheck == true)
+        {
+            MenuFadeOut();
+        }
+
+        if (FinishFadeCheck == true)
+        {
+            FinishFadeOut();
+        }
 
     }
 
     public void TitleScene()
     {
         GameObject.FindObjectOfType<AudioSource>().PlayOneShot(SelectSE);
-        SceneManager.LoadScene("TitleScene");
+        TitleFadeCheck = true;
+            
+        
     }
+
+    void TitleFadeOut()
+    {
+        Fade.GetComponent<Fade>().FadeOut();
+        FadeTime++;
+        if (FadeTime > FadeTimeMax)
+        {
+            FadeTime = 0;
+            TitleFadeCheck = false;
+            SceneManager.LoadScene("TitleScene");
+        }
+    }
+    void MenuFadeOut()
+    {
+        Fade.GetComponent<Fade>().FadeOut();
+        FadeTime++;
+        if (FadeTime > FadeTimeMax)
+        {
+            FadeTime = 0;
+            MenuFadeCheck = false;
+            SceneManager.LoadScene("MenuScene");
+        }
+    }
+
     public void MenuScene()
     {
         GameObject.FindObjectOfType<AudioSource>().PlayOneShot(SelectSE);
-        SceneManager.LoadScene("MenuScene");
+        MenuFadeCheck = true;
     }
     public void GameFinish()
     {
 
-        #if UNITY_EDITOR
-                    UnityEditor.EditorApplication.isPlaying = false;
-        #elif UNITY_WEBPLAYER
-        		Application.OpenURL("http://www.yahoo.co.jp/");
-        #else
-        		Application.Quit();
-        #endif
+        FinishFadeCheck = true;
         
+    }
+
+    void FinishFadeOut()
+    {
+        Fade.GetComponent<Fade>().FadeOut();
+        FadeTime++;
+        if (FadeTime > FadeTimeMax)
+        {
+            FadeTime = 0;
+            FinishFadeCheck = false;
+            #if UNITY_EDITOR
+                        UnityEditor.EditorApplication.isPlaying = false;
+            #elif UNITY_WEBPLAYER
+                    		Application.OpenURL("http://www.yahoo.co.jp/");
+            #else
+                    		Application.Quit();
+            #endif
+        }
+
     }
     public void Cancel()
     {
@@ -61,5 +121,17 @@ public class GameController : MonoBehaviour
         GameObject.FindObjectOfType<AudioSource>().PlayOneShot(SelectSE);
         DontDestroyOnLoad(SelectSE);
         SceneManager.LoadScene("SampleScene");
+    }
+
+    void TutorialFadeOut()
+    {
+        Fade.GetComponent<Fade>().FadeOut();
+        FadeTime++;
+        if (FadeTime > FadeTimeMax)
+        {
+            FadeTime = 0;
+            TutorialFadeCheck = false;
+            SceneManager.LoadScene("SampleScene");
+        }
     }
 }
