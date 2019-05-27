c872bb25cb399eb73527383c313700dd3d7b5077
SeiyaTakasugi <seiya100dash@gmail.com>
2019-05-27 10:27:38 +0900

    爆発エフェクトの実装

---
+++ WUS/WUS/Assets/script/ExplotionOnly.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplotionOnly : MonoBehaviour
+{
+    public GameObject ExploadObj;
+    public GameObject ExploadPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(ExploadObj, ExploadPos.transform.position, Quaternion.identity);
+    }
+}
