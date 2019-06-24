2b93b0d906a2b74fbcd5c354bc8a89f6211d241d
konsora <34297567+konsora910@users.noreply.github.com>
2019-06-24 11:04:09 +0900

    Revert "meta"
    This reverts commit b6f5d459a868dd79d370f306f502f9e33b3b88b5.

---
+++ WUS/WUS/Assets/cngSprite.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cngSprite : MonoBehaviour
+{
+
+    public Sprite sprite1;
+    public Sprite sprite2;
+
+    public GameObject stCont;
+    StageSelectUIControll SSUC;
+    
+    private int flag = 0;
+
+    private void Start()
+    {
+        SSUC = stCont.GetComponent<StageSelectUIControll>();
+    }
+    
+    public void ChangeSprite()
+    {
+        switch (flag)
+        {
+            case 0:
+                this.gameObject.GetComponent<Image>().sprite = sprite2;
+                flag = 1;
+                break;
+            case 1:
+                SSUC.test(false);
+                this.gameObject.GetComponent<Image>().sprite = sprite1;
+                flag = 0;
+                break;
+            default:
+                break;
+        }
+    }
+}
