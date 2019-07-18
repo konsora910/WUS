f6e541bbf263117409eb1c92512f3625d77a593e
konsora <34297567+konsora910@users.noreply.github.com>
2019-07-18 11:28:04 +0900

    修正

--- WUS/WUS/Assets/script/System/StageCheck.cs
+++ WUS/WUS/Assets/script/System/StageCheck.cs
@@ -15,7 +15,6 @@ public class StageCheck : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(NowStage);
     }
 
     public static void ClearStage1()
